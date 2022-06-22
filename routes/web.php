5ae1c6243547c235661d89e996c9af8114a3952d
Felipe Silva <felipe.silvads@outlook.com>
2022-06-22 20:16:10 -0300

    Criado o VagaCotroller

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\VagaController;
 
 /*
 |--------------------------------------------------------------------------
@@ -16,3 +17,6 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('layouts.base');
 });
+
+Route::resource('vagas', VagaController::class);
+
